e16c7301b54e3329c060c83ed16738e5bfec99dc
Nesar Ahmed <nesar13@hotmail.com>
2020-02-23 18:59:36 -0500

    Update Util.java

--- app/src/main/java/com/example/contactme/util/Util.java
+++ app/src/main/java/com/example/contactme/util/Util.java
@@ -1,4 +1,18 @@
 package com.example.contactme.util;
 
 public class Util {
+
+    //Database Related items so we don't have to update the db handler each time
+
+    public static final int DATABASE_VERSION=1;
+    public static final String DATABASE_NAME="contact_db";
+    public static final  String TABLE_NAME="contacts";
+
+    //Contacts table columns names
+    public static final String KEY_ID="id";
+    public static final String KEY_NAME="name";
+    public static final String KEY_PHONE_NUMBER="phone_number";
+
+    
+
 }
